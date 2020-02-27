9f4b26d11545d9a6a3a000d961e5888950b66e63
unknown <karta134033@gmail.com>
2020-02-27 11:54:09 +0800

    2020/02/27更新 Details: 新增viewSet、權限控制、genericViewSet 等

--- crawler/urls.py
+++ crawler/urls.py
@@ -1,8 +1,23 @@
-from django.urls import path
-from .views import article_list, article_detail, ArticleAPIView
+from django.urls import path, include
+from .views import article_list, article_detail, ArticleAPIView, ArticleDetails, GenericAPIView, ArticleViewSet, ArticleGenericViewSet
+from rest_framework.routers import DefaultRouter  # 給ArticleViewSet 註冊用
+
+router = DefaultRouter()
+router.register('article', ArticleViewSet, basename='article')  # 後方不用再加斜線
+router.register('generic/article', ArticleGenericViewSet, basename='generic_article')
+
 
 urlpatterns = [
+    path('viewset/', include([
+        path('', include(router.urls)),
+        # path('<int:pk>/', include(router.urls))  # 無此段還是找的到該id
+    ])),
     # path('article/', article_list),
     path('article/', ArticleAPIView.as_view()),  # 因為是傳入class 所以要再加上as_view
-    path('detail/<int:pk>', article_detail),
+    # path('detail/<int:pk>', article_detail),
+    path('detail/<int:id>', ArticleDetails.as_view()),
+    path('generic/article/', include([
+      path('', GenericAPIView.as_view()),
+      path('<int:id>/', GenericAPIView.as_view()),  # 若無此段會page not found
+    ])),
 ]
--- crawler/views.py
+++ crawler/views.py
@@ -8,6 +8,100 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
+from rest_framework import generics, mixins
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+from rest_framework import viewsets
+from django.shortcuts import get_object_or_404
+
+#---------------------------版本5-------------------------#
+
+class ArticleGenericViewSet(
+        viewsets.GenericViewSet,
+        mixins.ListModelMixin,
+        mixins.CreateModelMixin,
+        mixins.RetrieveModelMixin,
+        mixins.UpdateModelMixin,
+        mixins.DestroyModelMixin):
+
+    serializer_class = ArticleSerializer
+    queryset = Article.objects.all()
+
+    def post(self, request):
+        return self.create(request)
+
+    def post(self, request):
+        return self.create(request)
+
+    def delete(self, request, id):
+        return self.destroy(request, id)
+
+
+#---------------------------版本4-------------------------#
+
+class ArticleViewSet(viewsets.ViewSet):  # viewsets.ViewSet 需要將以下function寫出
+                                         # viewsets 需要以register的方式註冊
+    def list(self, request):
+        articles = Article.objects.all()
+        serializer = ArticleSerializer(articles, many=True)  # 重點:many=true
+        return Response(serializer.data)
+
+    def create(self, request):  # put
+        serializer = ArticleSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self, request, pk=None):
+        queryset = Article.objects.all()
+        article = get_object_or_404(queryset, pk=pk)
+        serializer = ArticleSerializer(article)
+        return Response(serializer.data)
+
+    def update(self, request, pk=None):
+        article = Article.objects.get(pk=pk)
+        serializer = ArticleSerializer(article, data=request.data)  # 指定更新某個article
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+#---------------------------版本3-------------------------#
+
+class GenericAPIView(
+        generics.GenericAPIView, 
+        mixins.ListModelMixin, 
+        mixins.CreateModelMixin,
+        mixins.UpdateModelMixin,
+        mixins.RetrieveModelMixin,  # 需有Retrieve才能綁定Update
+        mixins.DestroyModelMixin):
+
+    serializer_class = ArticleSerializer
+    queryset = Article.objects.all()
+    lookup_field = 'id'  # 加此段才能正確put資訊
+    authentication_classes = [SessionAuthentication, BasicAuthentication]  # 先確定Session認證 若無則再確認Basic認證
+    # authentication_classes = [TokenAuthentication]  # 在/admin中新增  最後在postman->Header->key: Authorization value:Token ....
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, id=None):
+        # 不用在get post等方法中去調用Article.objects.all()
+        if id: return self.retrieve(request)
+        else: return self.list(request)
+        return self.list(request)
+
+    def post(self, request):
+        return self.create(request)
+
+    def put(self, request, id=None):
+        return self.update(request, id)
+
+    def delete(self, request, id):
+        return self.destroy(request, id)
+
+
+#---------------------------版本2-------------------------#
 
 class ArticleAPIView(APIView):
 
@@ -22,7 +116,35 @@ class ArticleAPIView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        
+
+
+class ArticleDetails(APIView):
+
+    def get_object(self, id):
+        try:
+            return Article.objects.get(id=id)
+        except Article.DoesNotExist:
+            return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+
+    def get(self, request, id):
+        article = self.get_object(id)
+        serializer = ArticleSerializer(article)  # 因為只要單個，所以不用設定為many=true
+        return Response(serializer.data)  # 也不用加save
+
+    def put(self, request, id):
+        article = self.get_object(id)
+        serializer = ArticleSerializer(article, data=request.data)  # 指定更新某個article
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, id):
+        article = self.get_object(id)
+        article.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+#---------------------------版本1-------------------------#
 
 @api_view(['GET', 'POST'])
 def article_list(request):
@@ -57,10 +179,6 @@ def article_detail(request, pk):
     elif request.method == 'DELETE':
         article.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
-
-
-
-
 
 
 
